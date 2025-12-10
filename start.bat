489e1ef3929de0622e0bfc7429d58016732eb6f9
Edin O. <22921456+edinosma@users.noreply.github.com>
2025-12-10 17:44:32 -0500

    Create module for Oxxius LaserBoxx Laser Controllers

---
+++ start.bat
@@ -0,0 +1,3 @@
+%WINDIR%\System32\cmd.exe "/K" C:\ProgramData\anaconda3\Scripts\activate.bat C:\Shared\storm-control\.conda
+cd C:\Shared\storm-control\storm_control\hal4000\
+python hal4000.py C:\Shared\storm-control\storm_control\hal4000\xml\dutta_none_config.xml
